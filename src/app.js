beb8841a0a647e51ab830ac12d8830f49073b667
Manuel <manulm27@hotmail.com>
2022-06-09 01:32:32 +0200

    Finish project generate domain

--- src/app.js
+++ src/app.js
@@ -6,6 +6,31 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  var pronoun = ["the", "our"];
+  var adj = ["great", "big"];
+  var noun = ["jogger", "aried"];
+  var gateway = [".com", ".net", ".us", ".io", ".ed"];
+  var link = [];
+
+  for (var i = 0; i < pronoun.length; i++) {
+    for (var u = 0; u < adj.length; u++) {
+      for (var x = 0; x < noun.length; x++) {
+        for (var y = 0; y < gateway.length; y++) {
+          if (
+            noun[x].charAt(noun[x].length - 2) === "e" &&
+            noun[x].charAt(noun[x].length - 1) === "d" &&
+            gateway[y] === ".ed"
+          ) {
+            var new_word = noun[x].substring(0, noun[x].length - 2);
+            console.log(gateway[y]);
+            link.push(pronoun[i] + adj[u] + new_word + gateway[y]);
+          } else {
+            link.push(pronoun[i] + adj[u] + noun[x] + gateway[y]);
+          }
+        }
+      }
+    }
+  }
+
+  console.log(link);
 };
